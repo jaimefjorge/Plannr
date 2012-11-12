2a5e03917314e15c46be12cd114c9de5191a995d
Romain Bourse <bourseromain@gmail.com>
2012-11-12 17:53:58 +0100

    début admin

---
+++ Plannr/Models/Administrateur.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Plannr.Models
+{
+    public class Administrateur : Enseignant
+    {
+        public DateTime AdminDepuis { get; set; }
+
+
+    }
+}
