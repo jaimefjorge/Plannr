d8a45bc7ad60fb8c68e39eec25d6ad87cef5f2b7
Clément Balestrat <clement.balestrat@gmail.com>
2012-11-22 01:16:11 +0100

    Mon compte

---
+++ Plannr/Controllers/OptionController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Plannr.Controllers
+{
+    public class OptionController : Controller
+    {
+        //
+        // GET: /Option/
+
+        public ActionResult Index()
+        {
+            if (!Request.IsAjaxRequest())
+            {
+                return View();
+            }
+            else
+            {
+
+                return PartialView("_Index");
+            }
+        }
+
+        public ActionResult ChangeData()
+        {
+            return View();
+        }
+
+
+        public ActionResult ChangeUserName()
+        {
+            return View();
+        }
+
+        public ActionResult ChangePwd()
+        {
+            return View();
+        }
+
+
+
+    }
+}
